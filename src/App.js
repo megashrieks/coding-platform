15efefa2e5c3ac6e20b8e77db2449469b1efc491
acnologia <shrikanthbudya@gmail.com>
2018-05-27 19:24:23 +0530

    added the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import Main from './components/Main/Main';
+import Sidebar from './components/Sidebar/Sidebar';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Coding app
+        <Sidebar />
+        <Main/>
       </div>
     );
   }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Main.css';
+class Main extends Component {
+    render() {
+        return <div className="main">main</div>
+    }
+}
+export default Main;
