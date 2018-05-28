e0f35174df92b6d65d4e907194b66107fb5e4c39
acnologia <shrikanthbudya@gmail.com>
2018-05-28 14:47:54 +0530

    added the additional info button.

--- src/components/ContestItem/ContestItem.js
+++ src/components/ContestItem/ContestItem.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-
+import './ContestItem.css';
 class ContestItem extends Component {
+    state = {
+        active: false
+    };
+    toggleInfo = () => {
+        this.setState((prevState) =>  ({ active: !prevState.active }));
+    }
     render() {
+        var active = this.state.active ? " active" : "";
         return (
-            <div className={"contest " + this.props.type}>
+            <div className={"contest " + this.props.type+active}>
                 <div className="contest-title">
                     {this.props.title}
                 </div>
                 <div className="contest-details">
                     {this.props.details}
+                </div>
+                <div className="info-toggle" onClick={this.toggleInfo}>
+                    <i className = "fa fa-info-circle"></i>
+                </div>
+                <div className="additional-info">
+                    {this.props.additionalDetails}
                 </div>
             </div>
         );
