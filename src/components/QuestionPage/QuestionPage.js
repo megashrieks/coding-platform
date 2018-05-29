c76b6b32950cb9e44561a0e07770ee9100e1ed81
acnologia <shrikanthbudya@gmail.com>
2018-05-29 16:17:16 +0530

    misc changes.

--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component, Fragment } from 'react';
 import './QuestionPage.css';
 import Timer from './Timer/Timer';
@@ -10,46 +11,51 @@ class QuestionPage extends Component{
         let contestId = this.props.match.params.contestId;
         axios.get('http://localhost:5000/api/contests/'+contestId+'/questions')
             .then((data) => {
-                console.log(data.data);
-                if (data.data.length == 0)
+                if (data.data.length === 0)
                     console.log('no questions');
                 else
-                    this.setState({ data: data.data })
+                    this.setState({
+                        ...this.state,
+                        data: {
+                            ...this.state.data,
+                            questions:data.data
+                        }
+                    })
             })
             .catch(data => console.log);
-        this.setState({
-            data: {
-                contestName:"codex",
-                timeRemaining: [
-                    0/*days*/,
-                    0/*hours*/,
-                    0/*minutes*/,
-                    5/*seconds*/
-                ],
-                questions: [{
-                    title: "n-th fibonacci numbers",
-                    details: "Find the nth fibonacci number",
-                    solved: 'full',
-                    solvedBy: 0,
-                    difficulty:78
-                }, {
-                    title: "Square",
-                    details: "Find the area of the square",
-                    solved: 'partial',
-                    solvedBy: 108,
-                    difficulty: 5
-                }, {
-                    title: "pascals triangle",
-                    details: "print the pascal triangle",
-                    solved: 'no',
-                    solvedBy: 10,
-                    difficulty: 20
-                }]
-            }
-        })
+        // this.setState({
+        //     data: {
+        //         contestName:"codex",
+        //         timeRemaining: [
+        //             0/*days*/,
+        //             0/*hours*/,
+        //             0/*minutes*/,
+        //             5/*seconds*/
+        //         ],
+        //         questions: [{
+        //             title: "n-th fibonacci numbers",
+        //             details: "Find the nth fibonacci number",
+        //             solved: 'full',
+        //             solvedBy: 0,
+        //             difficulty:78
+        //         }, {
+        //             title: "Square",
+        //             details: "Find the area of the square",
+        //             solved: 'partial',
+        //             solvedBy: 108,
+        //             difficulty: 5
+        //         }, {
+        //             title: "pascals triangle",
+        //             details: "print the pascal triangle",
+        //             solved: 'no',
+        //             solvedBy: 10,
+        //             difficulty: 20
+        //         }]
+        //     }
+        // })
     }
     render() {
-        var questions = this.state.data.questions !== undefined &&
+        var questions = this.state.data.questions !== undefined ?
             this.state.data.questions.map((element, index) => {
                 return (
                     <div
@@ -59,7 +65,7 @@ class QuestionPage extends Component{
                         {element.title}
                     </div>
                 )
-            });
+            }):null;
         return (
             <Fragment>
                 <h1>
