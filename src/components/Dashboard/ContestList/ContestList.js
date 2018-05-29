a95923c25077782f5e7534662d6985e05806141c
acnologia <shrikanthbudya@gmail.com>
2018-05-29 14:09:48 +0530

    minor changes.

--- src/components/Dashboard/ContestList/ContestList.js
+++ src/components/Dashboard/ContestList/ContestList.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
 import axios from 'axios';
-
+import React, { Component } from 'react';
 import ContestItem from '../ContestItem/ContestItem';
 import './ContestList.css';
+
 class ContestList extends Component{
     constructor(props) {
         super(props);
@@ -11,44 +11,9 @@ class ContestList extends Component{
         };
     }
     componentDidMount() {
-
-        //////////////////////
-
         axios.get('http://localhost:5000/api/contests')
         .then((data) => this.setState({data: data.data}))
         .catch((err) => console.log(err));
-        
-
-        //////////////////
-        // this.setState({
-        //     data: [
-        //         {
-        //             title: "November Challenge",
-        //             type: "ongoing",
-        //             details: "Monthly challenge in this site for November",
-        //             additionalDetails: "This contest is in progress",
-        //             linkId:"1"
-        //         }, {
-        //             title: "October Challenge",
-        //             type: "mock",
-        //             details: "Monthly challenge in this site for October",
-        //             additionalDetails: "This contest has already ended",
-        //             linkId: "2"
-        //         }, {
-        //             title: "December Challenge",
-        //             type: "future",
-        //             details: "Monthly challenge in this site for December",
-        //             additionalDetails: "This contest will start on december",
-        //             linkId: "3"
-        //         }, {
-        //             title: "Codex 2k18 Challenge",
-        //             type: "ongoing",
-        //             details: "Coding Challenge for all the good programmers out there",
-        //             additionalDetails: "This contest is in progress",
-        //             linkId: "4"
-        //         }
-        //     ]
-        // });
     }
     render() {
         let contestListElements = this.state.data.map((element, index) => {
--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import React, { Component, Fragment } from 'react';
 
 class QuestionPage extends Component{
+    state = {
+        data:[]
+    };
+    componentDidMount() {
+        let contestId = this.props.match.params.contestId;
+        axios.get('http://localhost:5000/api/contests/'+contestId+'/questions')
+            .then((data) => this.setState({data:data.data}))
+            .catch(data => console.log);
+    }
     render() {
+        var questions = this.state.data.map((element, index) => {
+            return <li>{element.title}</li>
+        });
         return (
             <Fragment>
-                QuestionPage  {this.props.match.params.contestId}
+                Questions:
+                {questions}
             </Fragment>
         );
     }
