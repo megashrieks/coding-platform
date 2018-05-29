244cf881f1c525d05a37be67e6e07af9939b87c6
acnologia <shrikanthbudya@gmail.com>
2018-05-29 15:42:41 +0530

    made contest timer in QuestionPage component.

--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -1,23 +1,65 @@
-import axios from 'axios';
 import React, { Component, Fragment } from 'react';
-
+import './QuestionPage.css';
+import Timer from './Timer/Timer';
 class QuestionPage extends Component{
     state = {
-        data:[]
+        data: {},
+        once:true
     };
     componentDidMount() {
-        let contestId = this.props.match.params.contestId;
-        axios.get('http://localhost:5000/api/contests/'+contestId+'/questions')
-            .then((data) => this.setState({data:data.data}))
-            .catch(data => console.log);
+        // let contestId = this.props.match.params.contestId;
+        // axios.get('http://localhost:5000/api/contests/'+contestId+'/questions')
+        //     .then((data) => this.setState({data:data.data}))
+        //     .catch(data => console.log);
+        this.setState({
+            data: {
+                contestName:"codex",
+                timeRemaining: [
+                    0/*days*/,
+                    0/*hours*/,
+                    0/*minutes*/,
+                    5/*seconds*/
+                ],
+                questions: [{
+                    title: "n-th fibonacci numbers",
+                    details: "Find the nth fibonacci number",
+                    solved: 'full',
+                    solvedBy: 0,
+                    difficulty:78
+                }, {
+                    title: "Square",
+                    details: "Find the area of the square",
+                    solved: 'partial',
+                    solvedBy: 108,
+                    difficulty: 5
+                }, {
+                    title: "pascals triangle",
+                    details: "print the pascal triangle",
+                    solved: 'no',
+                    solvedBy: 10,
+                    difficulty: 20
+                }]
+            }
+        })
     }
     render() {
-        var questions = this.state.data.map((element, index) => {
-            return <li>{element.title}</li>
-        });
+        var questions = this.state.data.questions !== undefined &&
+            this.state.data.questions.map((element, index) => {
+            return (
+                <div
+                    className={"question "+element.solved}
+                    key={"question" + index}
+                    >
+                    {element.title}
+                </div>
+            )
+            });
         return (
             <Fragment>
-                Questions:
+                <h1>
+                    {this.state.data.contestName || "Loading contest details.."}
+                </h1>
+                {this.state.data.timeRemaining && <Timer timer={this.state.data.timeRemaining} />}
                 {questions}
             </Fragment>
         );
---
+++ src/components/QuestionPage/Timer/Timer.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './Timer.css';
+class Timer extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            once:true,
+            timeRemaining: this.props.timer
+        };
+        console.log(props)
+    }
+    decrementTimer = () => {
+        let time = this.state.timeRemaining;
+        let flag = false;
+        time[3]--;
+        if (time[0] < 1 && time[1] < 1 && time[2] < 1 && time[3] < 1) { clearInterval(2); flag = true; }
+        if (time[3] < 1) {
+            time[2]--; time[3] = 60;
+            if (time[2] < 0) {
+                time[1]--; time[2] = 60;
+                if (time[1] < 0) {
+                    time[0]--; time[1] = 24;
+                    if (time[0] < 0) time[0] = 0;
+                }
+            }
+        }
+        if (flag === true) {
+            time[0] = time[1] = time[2] = time[3] = 0;
+        }
+        this.setState({
+            ...this.state,
+            once: false,
+            timeRemaining: time
+        })
+    }
+    render() {
+        this.state.once &&
+            this.state.timeRemaining !== undefined &&
+            setInterval(() => { this.decrementTimer() }, 1000);
+        let timingDetails = {
+            days: this.state.timeRemaining !== undefined &&
+                this.state.timeRemaining[0] !== 0,
+            hours: this.state.timeRemaining !== undefined &&
+                this.state.timeRemaining[1] !== 0,
+            minutes: this.state.timeRemaining !== undefined &&
+                this.state.timeRemaining[2] !== 0,
+            seconds: this.state.timeRemaining !== undefined &&
+                this.state.timeRemaining[3] !== 0
+        }
+        return <div className="time-remaining">
+            {
+                (timingDetails.days ||
+                    timingDetails.hours ||
+                    timingDetails.minutes ||
+                    timingDetails.seconds) ? "Contest ends in : " : <b>contest has ended</b>
+            }
+            {
+                timingDetails.days &&
+                (" " + this.state.timeRemaining[0] + "d")
+            }
+            {
+                timingDetails.hours &&
+                (" " + this.state.timeRemaining[1] + "h")
+            }
+            {
+                timingDetails.minutes &&
+                (" " + this.state.timeRemaining[2] + "m")
+            }
+            {
+                timingDetails.seconds &&
+                (" " + this.state.timeRemaining[3] + "s")
+            }
+        </div>
+    }
+}
+export default Timer;
